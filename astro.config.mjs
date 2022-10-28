a837d8f9af03b8dc89cbe918932159b6fcee55f5
RoyEden <roy.ede@gmail.com>
2022-10-28 13:09:56 -0300

    * Updated dependencies. * Updated to edge runtime.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
