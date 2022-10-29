acc9e7faeca9c534010d0a01e5eeff484d266b36
RoyEden <roy.ede@gmail.com>
2022-10-29 15:17:26 -0300

    Fix deploy, edge is not ready yet

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
